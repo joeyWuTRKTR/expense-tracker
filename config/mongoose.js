6f45ec00f7745ff54d2348808730ff1c539ddda8
joeyWuTRKTR <godatascial@yahoo.com>
2021-07-19 21:44:56 +0800

    feat: add mongoose on config/mongoose.js

---
+++ config/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+mongoose.connected('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => console.log('MongoDB connect error!'))
+
+db.once('open', () => console.log('MongoDB Connected!'))
+
+module.exports = db
