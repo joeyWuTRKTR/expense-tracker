bbaada07af2978b8e2f960b7d35afe3a23077720
joeyWuTRKTR <godatascial@yahoo.com>
2021-07-20 21:35:06 +0800

    feat: add layout and partial template

---
+++ routes/modules/expense.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const Expense = require('../../models/expenses')
+const router = express.Router()
+
+router.get('/new', (req, res) => {
+  return Expense.find()
+    .lean()
+    .then(expenses => res.render('new', { expenses }))
+    .catch(err => console.log(err))
+})
+
+router.get('/edit', (req, res) => {
+  return Expense.find()
+    .lean()
+    .then(expenses => res.render('edit', { expenses }))
+    .catch(err => console.log(err))
+})
+
+module.exports = router
