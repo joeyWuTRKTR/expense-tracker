d1eeace77ba8caa96acca7eba74dab15498857e0
joeyWuTRKTR <godatascial@yahoo.com>
2021-07-22 16:39:19 +0800

    feat: change layout of create page

--- routes/modules/expense.js
+++ routes/modules/expense.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const Record = require('../../models/record')
+const Category = require('../../models/category')
 const router = express.Router()
 
 // Create - lead to create page
-router.get('/new', (req, res) => {
+router.get('/new', async (req, res) => {
+  const categories = await Category.find().lean()
   return Record.find()
     .lean()
-    .then(() => res.render('new'))
+    .then(() => res.render('new', { categories }))
     .catch(err => console.log(err))
 })
 
 // Create - lead to index page
 router.post('/', (req, res) => {
   const { name, category, date, amount } = req.body
-  console.log(`name: ${name}, category: ${category}, amount: ${amount}`)
   return Record.create({ name, category, date, amount })
     .then(() => res.redirect('/'))
     .catch(err => console.log(err))
