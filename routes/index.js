32180dedc98a071676b0e6ef3d2148fc7637e70b
joeyWuTRKTR <godatascial@yahoo.com>
2021-08-10 18:03:47 +0800

    feat: add login/register router & partial template

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const expense = require('./modules/expense')
+const user = require('./modules/user')
 
 router.use('/', home)
 router.use('/expense', expense)
+router.use('/user', user)
 
 module.exports = router
