9e236f732399ea2aa712c69b651a34927b112dd4
joeyWuTRKTR <godatascial@yahoo.com>
2021-08-10 22:37:29 +0800

    feat: add isAuthenticated to display different template views

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
-const expense = require('./modules/expenses')
-const user = require('./modules/users')
+const expenses = require('./modules/expenses')
+const users = require('./modules/users')
 const { authenticator } = require('../middleware/auth')
 
-router.use('/expenses', authenticator, expense)
-router.use('/users', user)
+router.use('/expenses', authenticator, expenses)
+router.use('/users', users)
 router.use('/', authenticator, home)
 
 module.exports = router
