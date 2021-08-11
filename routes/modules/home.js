424cb77c6e5b9d0b49cbbae0f3f4479cad5b440a
joeyWuTRKTR <godatascial@yahoo.com>
2021-08-11 11:47:23 +0800

    feat: add month-dropdown filter

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -27,27 +27,63 @@ router.get('/', async(req, res) => {
     .catch(err => console.log(err))
 })
 
-// 依照分類渲染index頁面
+// 類別分類
+// router.get('/filter', async (req, res) => {
+//   const categoryName = req.query.category // 找到回傳的分類
+//   const category = await Category.findOne({ categoryName }) // 在資料庫找到該分類的所有資料
+//   const categories = await Category.find().lean()
+
+//   if (!category) return res.redirect('/') // 點選"所有分類"，找不到categoryname所以返回主頁面
+  
+//   return Record.find({ category: category.categoryName }).lean()
+//     .then(records => {
+//       let totalAmount = 0
+//       records.map(record => {
+//         totalAmount += record.amount
+//         record.date = dateToString(record.date)
+//         record.categoryIcon = category.categoryIcon
+//       })
+//       res.render('index', { records, totalAmount, categories,
+//         currentCategory: category.categoryName })
+//     })
+//     // .then(res.redirect('/'))
+//     .catch()
+// })
+
 router.get('/filter', async (req, res) => {
-  const categoryName = req.query.category // 找到回傳的分類
-  const category = await Category.findOne({ categoryName }) // 在資料庫找到該分類的所有資料
+  const userId = req.user._id
+  const categoryFiltered = req.query.category
+  const monthFiltered = Number(req.query.month)
   const categories = await Category.find().lean()
 
-  if (!category) return res.redirect('/') // 點選"所有分類"，找不到categoryname所以返回主頁面
-  
-  return Record.find({ category: category.categoryName }).lean()
-    .then(records => {
+  // create mongoose aggregate filter query
+  const filterQuery = {
+    userId: userId,
+    isDelete: false
+  }
+  categoryFiltered ? filterQuery.category = categoryFiltered : ''
+  monthFiltered? filterQuery.month = monthFiltered : ''
+
+  Record.aggregate([
+    // 拉出需要的欄位
+    { $project: { name: 1, category: 1, date: 1, amount: 1, merchant: 1, userId: 1, isDelete: 1, month: { $month: '$date' } } },
+    // 篩選分類或者月份by filterQuery
+    { $match: filterQuery }
+  ])
+    .then(async (records) => {
+      // 加入分類圖案
+      const categoryData = {}
+      categories.forEach(category => categoryData[category.categoryName] = category.categoryIcon)
+
+      // 計算總金額 & 回傳資料
       let totalAmount = 0
       records.map(record => {
-        totalAmount += record.amount
         record.date = dateToString(record.date)
-        record.categoryIcon = category.categoryIcon
+        totalAmount += record.amount
+        record.categoryIcon = categoryData[record.category] // 用category資料在record裡新增fontawesome icon
       })
-      res.render('index', { records, totalAmount, categories,
-        currentCategory: category.categoryName })
+      return res.render('index', { records, totalAmount, categoryFiltered, categories, monthFiltered})
     })
-    // .then(res.redirect('/'))
-    .catch()
 })
 
 module.exports = router
