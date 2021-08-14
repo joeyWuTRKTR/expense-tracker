0c1063d9ad146ae45bd080a44c6f048eeb6daeb2
joeyWuTRKTR <godatascial@yahoo.com>
2021-08-14 10:49:25 +0800

    modify: change Promise to Async/Await in home.js

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,25 +6,25 @@ const router = express.Router()
 
 // 使用async/await function非同步處理category和record兩個種子資料
 router.get('/', async(req, res) => {
-  const userId = req.user._id
-  const categories = await Category.find().lean()
-  // 把分類和icon額外拉到物件裡
-  const categoryData = {}
-  categories.forEach(category => categoryData[category.categoryName] = category.categoryIcon)
+  try {
+    const userId = req.user._id
+    const categories = await Category.find().lean()
+    const records = await Record.find({ userId }).lean()
+    // 把分類和icon額外拉到物件裡
+    const categoryData = {}
+    categories.forEach(category => categoryData[category.categoryName] = category.categoryIcon)
 
-  return Record.find({ isDelete: false, userId })
-    .lean()
-    .then(records => {
-      let totalAmount = 0
-      records.map(record => {
-        totalAmount += record.amount
-        record.date = dateToString(record.date)
-        // 將category物件的值，傳入record中
-        record.categoryIcon = categoryData[record.category]
-      })
-      res.render('index', { records, totalAmount, categories })
+    let totalAmount = 0
+    records.forEach(record => {
+      totalAmount += record.amount
+      record.date = dateToString(record.date)
+      // 將category物件的值，傳入record中
+      record.categoryIcon = categoryData[record.category]
     })
-    .catch(err => console.log(err))
+    res.render('index', { records, totalAmount, categories })
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 // 複合式搜尋
@@ -33,6 +33,7 @@ router.get('/filter', async (req, res) => {
   const categoryFiltered = req.query.category
   const inputMonth = req.query.yearMonth
 
+  // #ne 用於找出不等於的資料
   const year = Number(inputMonth.split('-')[0]) ? Number(inputMonth.split('-')[0]) : { $ne: ''}
   const month = Number(inputMonth.split('-')[1]) ? Number(inputMonth.split('-')[1]) : { $ne: '' }
 
@@ -54,26 +55,38 @@ router.get('/filter', async (req, res) => {
   }
 
   // 複合式篩選資料
-  Record.aggregate([
+  const records = await Record.aggregate([
     // 拉出需要的欄位
     { $project: { name: 1, category: 1, date: 1, amount: 1, merchant: 1, userId: 1, isDelete: 1, year: { $year: '$date' }, month: { $month: '$date' } } },
     // 篩選分類或者月份by filterQuery
     { $match: filterQuery }
   ])
-    .then(async (records) => {
-      // 加入分類圖案
-      const categoryData = {}
-      categories.forEach(category => categoryData[category.categoryName] = category.categoryIcon)
 
-      // 計算總金額 & 回傳資料
-      let totalAmount = 0
-      records.forEach(record => {
-        record.date = convertDate(record.date)
-        totalAmount += record.amount
-        record.categoryIcon = categoryData[record.category] // 用category資料在record裡新增fontawesome icon
-      })
-      return res.render('index', { records, totalAmount, categories, categoryFiltered, inputMonth})
-    })
+  const categoryData = {}
+  categories.forEach(category => categoryData[category.categoryName] = category.categoryIcon)
+
+  // 計算總金額 & 回傳資料
+  let totalAmount = 0
+  records.forEach(record => {
+    record.date = convertDate(record.date)
+    totalAmount += record.amount
+    record.categoryIcon = categoryData[record.category] // 用category資料在record裡新增fontawesome icon
+  })
+  return res.render('index', { records, totalAmount, categories, categoryFiltered, inputMonth })
+    // .then(async (records) => {
+    //   // 加入分類圖案
+    //   const categoryData = {}
+    //   categories.forEach(category => categoryData[category.categoryName] = category.categoryIcon)
+
+    //   // 計算總金額 & 回傳資料
+    //   let totalAmount = 0
+    //   records.forEach(record => {
+    //     record.date = convertDate(record.date)
+    //     totalAmount += record.amount
+    //     record.categoryIcon = categoryData[record.category] // 用category資料在record裡新增fontawesome icon
+    //   })
+    //   return res.render('index', { records, totalAmount, categories, categoryFiltered, inputMonth})
+    // })
 })
 
 module.exports = router
