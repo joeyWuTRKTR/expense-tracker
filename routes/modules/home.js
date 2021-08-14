aab18ce29681d41162e93dd6daddfb87b314c9ad
joeyWuTRKTR <godatascial@yahoo.com>
2021-08-14 11:41:14 +0800

    refactor: add try/catch in Async/Await in home.js

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -22,57 +22,63 @@ router.get('/', async(req, res) => {
       record.categoryIcon = categoryData[record.category]
     })
     res.render('index', { records, totalAmount, categories })
-  } catch (err) {
+  } 
+  catch (err) {
     console.log(err)
   }
 })
 
 // 複合式搜尋
 router.get('/filter', async (req, res) => {
-  const userId = req.user._id
-  const categoryFiltered = req.query.category
-  const inputMonth = req.query.yearMonth
+  try {
+    const userId = req.user._id
+    const categoryFiltered = req.query.category
+    const inputMonth = req.query.yearMonth
 
-  // #ne 用於找出不等於的資料
-  const year = Number(inputMonth.split('-')[0]) ? Number(inputMonth.split('-')[0]) : { $ne: ''}
-  const month = Number(inputMonth.split('-')[1]) ? Number(inputMonth.split('-')[1]) : { $ne: '' }
+    // #ne 用於找出不等於的資料
+    const year = Number(inputMonth.split('-')[0]) ? Number(inputMonth.split('-')[0]) : { $ne: '' }
+    const month = Number(inputMonth.split('-')[1]) ? Number(inputMonth.split('-')[1]) : { $ne: '' }
 
-  const categories = await Category.find().lean()
+    const categories = await Category.find().lean()
 
-  // 將使用者id作為基本資料，依照選單選擇把使用者想要的資料塞入
-  const filterQuery = {
-    userId: userId,
-    isDelete: false
+    // 將使用者id作為基本資料，依照選單選擇把使用者想要的資料塞入
+    const filterQuery = {
+      userId: userId,
+      isDelete: false
+    }
+
+    if (categoryFiltered) {
+      filterQuery.category = categoryFiltered
+    }
+
+    if (inputMonth) {
+      filterQuery.year = year
+      filterQuery.month = month
+    }
+
+    // 複合式篩選資料
+    const records = await Record.aggregate([
+      // 拉出需要的欄位
+      { $project: { name: 1, category: 1, date: 1, amount: 1, merchant: 1, userId: 1, isDelete: 1, year: { $year: '$date' }, month: { $month: '$date' } } },
+      // 篩選分類或者月份by filterQuery
+      { $match: filterQuery }
+    ])
+
+    const categoryData = {}
+    categories.forEach(category => categoryData[category.categoryName] = category.categoryIcon)
+
+    // 計算總金額 & 回傳資料
+    let totalAmount = 0
+    records.forEach(record => {
+      record.date = convertDate(record.date)
+      totalAmount += record.amount
+      record.categoryIcon = categoryData[record.category] // 用category資料在record裡新增fontawesome icon
+    })
+    return res.render('index', { records, totalAmount, categories, categoryFiltered, inputMonth })
   }
-
-  if (categoryFiltered) {
-    filterQuery.category = categoryFiltered
+  catch (err) {
+    console.log(err)
   }
-
-  if (inputMonth) {
-    filterQuery.year = year
-    filterQuery.month = month
-  }
-
-  // 複合式篩選資料
-  const records = await Record.aggregate([
-    // 拉出需要的欄位
-    { $project: { name: 1, category: 1, date: 1, amount: 1, merchant: 1, userId: 1, isDelete: 1, year: { $year: '$date' }, month: { $month: '$date' } } },
-    // 篩選分類或者月份by filterQuery
-    { $match: filterQuery }
-  ])
-
-  const categoryData = {}
-  categories.forEach(category => categoryData[category.categoryName] = category.categoryIcon)
-
-  // 計算總金額 & 回傳資料
-  let totalAmount = 0
-  records.forEach(record => {
-    record.date = convertDate(record.date)
-    totalAmount += record.amount
-    record.categoryIcon = categoryData[record.category] // 用category資料在record裡新增fontawesome icon
-  })
-  return res.render('index', { records, totalAmount, categories, categoryFiltered, inputMonth })
 })
 
 module.exports = router
